a510797352a2c567376680a95bec812db9806af0
nlovesye <894765963@qq.com>
2019-03-25 09:41:36 +0800

    dev

--- middleware/auth/jwt.js
+++ middleware/auth/jwt.js
@@ -2,7 +2,7 @@ module.exports = () => {
     return async (ctx, next) => {
         let token = ((ctx.request.body && ctx.request.body.access_token) || (ctx.query && ctx.query.access_token) || (ctx.header['authorization']))
         // console.log('jwtconsole', ctx.request.body, ctx.query.access_token, ctx.header['x-access-token'])
-        if (ctx.url.startsWith('/v1.0/api/user/login')) {//如果是用户登录
+        if (ctx.url.startsWith('/api/user/login')) {//如果是用户登录
             if (token === 'Basic token') {
                 await next()
             } else {
